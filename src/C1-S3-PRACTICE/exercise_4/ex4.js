1cad7dda4e90833762a518f21779facd6cb08e18
Huỳnh Đức <duc.huynh25@student.passerellesnumeriques.org>
2024-04-15 12:29:10 +0700

    C1-S3-PRACTICE#Done:100%

--- src/C1-S3-PRACTICE/exercise_4/ex4.js
+++ src/C1-S3-PRACTICE/exercise_4/ex4.js
@@ -1,8 +1,27 @@
 const words = ['spray', 'elite', 'exuberant', 'destruction', 'present'];
 const numbers = [1,2,3,4,5];
+
 const filterEvenNumbers1 = (arr) => arr.filter(word => word.length > 6);
-const filterEvenNumbers2 = (arr) => arr.map(word => word.length > 6 ? word : null).filter(word => word !== null);
+function filterEvenNumbers2(arr){
+    return arr.filter(word => word.length > 6);
+}
+
+const filterEvenNumbers3 = (arr) => arr.map(word => word.length > 6 ? word : null).filter(word => word !== null);
+function filterEvenNumbers4(arr){
+    return arr.map(word => word.length > 6 ? word : null).filter(word => word !== null);
+}
+
 const squareNumbers = (arr) =>  arr.map(number => number * number);
-console.log(filterEvenNumbers1(words));
+function squareNumbers(arr) {
+    return arr.map(number => number * number);
+}
+
+const sumArray = (arr) => arr.reduce((total, num) => total + num);
+function sumArray (arr) {
+    return arr.reduce((total, num) => total + num);
+}
+
+console.log(filterEvenNumbers5(words));
 console.log(filterEvenNumbers2(words));
 console.log(squareNumbers(numbers));
+console.log(sumArray(numbers));
